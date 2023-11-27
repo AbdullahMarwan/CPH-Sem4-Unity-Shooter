3d4b53905dc5dc473c249c53b14b0d6732c58de4
MrSalim86 <cph-ms1068@cphbusiness.dk>
2023-11-27 15:24:39 +0100

    Change shot and add target

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,15 +6,39 @@ public class Weapon : MonoBehaviour
 {
 
     public Transform bulletSpawnPoint;
-    public GameObject bulletPrefab;
-    public float bulletSpeed = 300;
+    public float damage = 10f;
+    public float impactForce = 100f;
+    public float range = 100f;
+    public float firingRate = 15f;
 
-    void Update()
+    public Camera FPSCam;
+    private float nextTimeToFire = 0f;
+
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire) // Checks if the player is holding down the left mouse button and if the current time is greater than or equal to the next time to fire
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            nextTimeToFire = Time.time + 1f / firingRate; // Sets the next time to fire to the current time plus 1 divided by the firing rate
+            Shoot(); // Calls the Shoot function
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit; // Creates a RaycastHit variable
+        if (Physics.Raycast(FPSCam.transform.position, FPSCam.transform.forward, out hit, range)) // Checks if the raycast hits something
+        {
+            Debug.Log(hit.transform.name);
+
+            if (hit.rigidbody != null) // Checks if the object that was hit has a rigidbody
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce); // Adds force to the rigidbody of the object that was hit
+            }
+            Target target = hit.transform.GetComponent<Target>(); // Gets the Target component of the object that was hit
+            if (target != null) // Checks if the object that was hit has a Target component
+            {
+                target.TakeDamage(damage); // Calls the TakeDamage function of the Target component
+            }
         }
     }
 }
---
+++ Assets/Target.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public bool isTargetPractice;
+    public float health = 10f;
+    public float defaultHealth;
+
+    private void Start()
+    {
+        defaultHealth = health;
+    }
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        if (isTargetPractice)
+        {
+            health = defaultHealth;
+            gameObject.transform.position = new Vector3(UnityEngine.Random.Range(-22, 22), UnityEngine.Random.Range(3, 15), UnityEngine.Random.Range(-23, -10));
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Debug.Log("Target Broken!");
+    }
+}
